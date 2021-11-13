9b758a8cbebfbba0cb6f2f27c766516983bb4d57
Tigran Avetisyan <tigran.1995.11@gmail.com>
2021-11-13 19:18:03 +0400

    Updating app

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+echo > logs.txt
 pip install -r api/requirements.txt
 python api/api.py &
 cd app/TPOT_web_app
@@ -5,10 +6,10 @@ npm install yarn
 npm install papaparse
 npm install http-server
 cd -
-/git-bash.exe ./server.sh &
+http-server ./ --cors -c-1 -s &
 cd app/TPOT_web_app
 yarn install
-yarn start
+yarn start && fg
 npx kill-port 3000
 npx kill-port 5000
 npx kill-port 8080
