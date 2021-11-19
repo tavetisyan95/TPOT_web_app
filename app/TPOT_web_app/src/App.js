0ec6033a5c572afe944fbba968a68c1feb3db52a
Tigran Avetisyan <tigran.1995.11@gmail.com>
2021-11-19 14:26:28 +0400

    Neatening the code up

--- app/TPOT_web_app/src/App.js
+++ app/TPOT_web_app/src/App.js
@@ -5,18 +5,18 @@ import UI from "./UI.js";
 import {events} from "./events.js";
 
 function App() {
-  return (
-    <>
-    <div className="App">
-      <p className="title">TPOT TRAINER</p>
-      <p className="subtitle">Select parameters for TPOT training.</p>
-	  
-	  <UI/>
-	  	  
-	  <button onClick={(event) => {events.trainTPOT()}}>Train</button>                	  
-    </div>
-    </>
-  );
+	return (
+		<>
+			<div className="App">
+				<p className="title">TPOT TRAINER</p>
+				<p className="subtitle">Select parameters for TPOT training.</p>
+				
+				<UI/>
+			
+				<button onClick={(event) => {events.trainTPOT()}}>Train</button>                	  
+			</div>
+		</>
+	);
 }
 
 export default App;
