44177711f7b6d1064e7d8ebba39e04fa376290eb
Tigran Avetisyan <tigran.1995.11@gmail.com>
2021-11-20 01:29:53 +0400

    Updating shell script

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 pip install -r api/requirements.txt
 python api/api.py &
 cd app/TPOT_web_app
-npm install --global yarn
-npm install papaparse
 npx http-server ./ --cors -c-1 -s &
-yarn install
-yarn start
+npm install papaparse
+npx yarn install
+npx yarn start
 echo > logs.txt
 if [ -f $"script.py" ]; then rm script.py; fi
 npx kill-port 3000
